802fd67d93c128295387eeaddab01afcae108911
Uliana Nadorozhna <unadorozhna1@gmail.com>
2023-09-18 14:39:33 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const refs = {
+  button : document.querySelector(".change-color"),
+  span : document.querySelector(".color"),
+  body: document.querySelector("body")
+}
+
+refs.button.addEventListener('click', () => {
+ const randomColor = getRandomHexColor();
+  refs.body.style.background = randomColor;
+  refs.span.textContent = randomColor;
+})
