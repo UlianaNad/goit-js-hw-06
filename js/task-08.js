cbcfa6047affb09cd4f75cd5d317e66eb24dd3ae
Uliana Nadorozhna <unadorozhna1@gmail.com>
2023-09-18 14:26:34 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
 
+const refs = {
+    form : document.querySelector('.login-form'),
+
+}
+
+refs.form.addEventListener('submit', (ev) => {
+    ev.preventDefault();
+
+    const emailValue = refs.form.elements.email.value;
+    const passwordValue = refs.form.elements.password.value;
+
+    if (emailValue && passwordValue){
+        let user = {
+            email:emailValue,
+            password: passwordValue,
+        }
+        console.log(user);
+    } else {
+        alert("Fill all fields!")
+    }
+});
